00ba2cacb3a68b25b2b5e40f55eeec73557cb265
devjosegomez <devjosegomez@gmail.com>
2019-11-10 08:29:11 -0600

    Changed methods to static

--- src/com/jsp04declarations/model/HelloFromAclass.java
+++ src/com/jsp04declarations/model/HelloFromAclass.java
@@ -2,11 +2,11 @@ package com.jsp04declarations.model;
 
 public class HelloFromAclass {
 	
-	public String makeItUpperCase(String msg){
+	public static String makeItUpperCase(String msg){
 		return msg.toUpperCase();
 	}
 	
-	public String makeItLowerCase(String msg){
+	public static String makeItLowerCase(String msg){
 		return msg.toLowerCase();
 	}
 }
