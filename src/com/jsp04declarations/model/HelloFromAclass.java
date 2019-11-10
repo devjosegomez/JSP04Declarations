9199b816ec35ceea4c1a6de8ef0a93c1022a8e67
devjosegomez <devjosegomez@gmail.com>
2019-11-10 08:17:50 -0600

    Added makeItUpperCase(), makeItLowerCase()

---
+++ src/com/jsp04declarations/model/HelloFromAclass.java
@@ -0,0 +1,12 @@
+package com.jsp04declarations.model;
+
+public class HelloFromAclass {
+	
+	public String makeItUpperCase(String msg){
+		return msg.toUpperCase();
+	}
+	
+	public String makeItLowerCase(String msg){
+		return msg.toLowerCase();
+	}
+}
